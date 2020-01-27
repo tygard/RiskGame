c4ccc83c673ba69757c1cc970b3bd8e1255d3908
HeroicVillain <dragonock540@gmail.com>
2020-01-26 22:35:27 -0600

    Copy risk folder to my directory.

---
+++ Experiments/Jesse/risk/Test/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:risk/src/pages/homeScreen/homeScreenBoiler.dart';
+import 'package:risk/src/pages/homeScreen/LogoutScreen.dart';
+import 'package:risk/services/Account.dart';
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .then((_) {
+    runApp(new Risk());
+  });
+}
+
+class Risk extends StatelessWidget {
+  @override
+  UserAccount user = new UserAccount();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Risk',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF0E1E1E),
+        accentColor: const Color(0xFFFFC454),
+      ),
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+        '/': (_) => new HomeScreenBoiler(user),
+        '/home': (_) => new LogoutScreen(user),
+      }
+
+    );
+  }
+}
