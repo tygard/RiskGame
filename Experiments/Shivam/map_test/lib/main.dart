29735ec0bba4d825da674bbe1c71ee891785d38d
Shivam Vashi <svashi@iastate.edu>
2020-01-27 16:49:37 -0600

    Added image test

---
+++ Experiments/Shivam/map_test/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft,DeviceOrientation.landscapeRight]);
+    return Image(image: AssetImage('assets/map.png'));
+
+  }
+
+}
+
+
+
