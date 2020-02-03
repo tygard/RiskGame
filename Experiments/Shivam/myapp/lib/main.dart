48e6354c186d8ffa218f9a463d90f10dfe486854
Shivam Vashi <svashi@iastate.edu>
2020-02-03 17:04:20 -0600

    Tweaked Animation Experiment

--- Experiments/Shivam/myapp/lib/main.dart
+++ Experiments/Shivam/myapp/lib/main.dart
@@ -13,32 +13,54 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin{
 
   @override
   void initState() {
-    super.initState();
-    controller =
-        AnimationController(duration: const Duration(seconds: 5), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-      ..addListener(() {
-        setState(() {
-          // The state that has changed here is the animation object’s value.
-        });
-      });
-    controller.forward();
-  }
+         super.initState();
+         controller =
+             AnimationController(duration: const Duration(seconds: 2), vsync: this);
+         animation = Tween<double>(begin: 0, end: 300).animate(controller)
+           ..addListener(() {
+             setState(() {
+               // The state that has changed here is the animation object’s value.
+             });
+           });
+         controller.forward();
+       }
 
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
+    return Column(
+      children: [
+        Container(
         margin: EdgeInsets.symmetric(vertical: 10),
         height: animation.value,
         width: animation.value,
         child: FlutterLogo(),
       ),
+        new Container(
+          width: 320.0,
+          height: 60.0,
+          alignment: FractionalOffset.center,
+          decoration: new BoxDecoration(
+            color: const Color.fromRGBO(247, 64, 106, 1.0),
+            borderRadius: new BorderRadius.all(const Radius.circular(30.0)),
+          ),
+          child: new Text(
+            "Sign In", textDirection: TextDirection.ltr,
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 0.3,
 
+            ),
+          ),
+        )
+      ],
     );
+
+
   }
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+     void dispose() {
+         controller.dispose();
+         super.dispose();
+       }
 }
