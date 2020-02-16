72cf60558c4552f5aec6bd3cbe3d5704ad8332a7
tygard <tygardner12@gmail.com>
2020-02-15 21:25:32 -0600

    passives screen can now update passive objects and modify the corresponding widgets

--- Experiments/Ty/passive_screen/lib/main.dart
+++ Experiments/Ty/passive_screen/lib/main.dart
@@ -1,90 +1,102 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PassivesScreen());
+  runApp(PassiveScreen());
 }
 
-class PassivesScreen extends StatelessWidget {
-  var p1_desc = 'passive 1 text';
+class PassiveScreen extends StatefulWidget {
+  @override
+  _PassiveScreenState createState() => _PassiveScreenState();
+}
+
+enum teamColor {
+  red, yellow, blue, green
+}
+/**
+ * a passive is an object that modifies the game state,
+ * it has a cost, duration and one or more attributes (helpful only)
+ */
+class Passive {
+  String description = "default description";
+  int cost = 0;
+  int duration = 0;
+  bool isActive = false;
+  var teamColor;
+}
+
+class _PassiveScreenState extends State<PassiveScreen> {
+  var currentPassives = new List<Passive>();
+  var p = new Passive();
+  Color _backgroundColor = Colors.green;
+
+  /**
+   * TODO:
+   * purchasePassive needs access to the current users team color/name 
+   * to determine who the passive should be assigned to.
+   * Also updates the state of the FloatingActionButton colors and return value
+   */
+  void purchasePassive() {
+    setState(() {
+      if (p.isActive){
+        _backgroundColor = Colors.grey[600];
+        return null;
+      } 
+      else {
+      // assign a passive to the user
+      p.isActive = true;
+      }
+    });
+  }
+
+  void removePassive(){
+    setState(() {
+      // unassing a passive to the user
+      p.isActive = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber,
         body: Container(
+          // this container holds the entire widget tree for this screen
           constraints: BoxConstraints(
             maxHeight: 500,
           ),
           alignment: Alignment.bottomLeft,
           padding: EdgeInsets.all(0),
-          color: Colors.green,
+          color: Colors.blueGrey,
           child: ListView(
+            // this is the list of passive descriptions
             shrinkWrap: true,
             controller: ScrollController(initialScrollOffset: 0),
             children: <Widget>[
-              // use a loop to build appropriate num of passives with own 'passive' objects
               Container(
+                // tihs container is a row containing the passive description string and purchase button
                 constraints: BoxConstraints(
                   maxHeight: 30,
                 ),
-                color: Colors.indigoAccent,
+                color: Colors.blue,
                 alignment: Alignment.centerLeft,
                 padding: EdgeInsets.all(4),
                 child: Row(
                   children: <Widget>[
-                    Icon(Icons.contact_phone),
+                    Icon(Icons.assessment),
                     Text('\t'),
-                    Text(p1_desc),
+                    Text(p.description),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    FloatingActionButton(
+                      onPressed: purchasePassive, // call method to update user attributes
+                      shape: RoundedRectangleBorder(),
+                      child: Text("Buy"),
+                      backgroundColor: _backgroundColor,
+                    )
                   ],
                 ),
               ),
-              Container(
-                constraints: BoxConstraints(
-                  maxHeight: 30,
-                ),
-                color: Colors.cyan,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(4),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.contact_phone),
-                    Text('\t'),
-                    Text(p1_desc),
-                  ],
-                ),
-              ),
-              Container(
-                constraints: BoxConstraints(
-                  maxHeight: 30,
-                ),
-                color: Colors.cyan,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(4),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.contact_phone),
-                    Text('\t'),
-                    Text(p1_desc),
-                  ],
-                ),
-              ),
-              Container(
-                constraints: BoxConstraints(
-                  maxHeight: 30,
-                ),
-                color: Colors.cyan,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.all(4),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.contact_phone),
-                    Text('\t'),
-                    Text(p1_desc),
-                  ],
-                ),
-              ),
-              
             ],
           ),
         ),
