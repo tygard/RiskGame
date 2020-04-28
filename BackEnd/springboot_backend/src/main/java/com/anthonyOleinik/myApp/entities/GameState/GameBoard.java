ad1870348187c30a015cfff7c1a06c09bd61d46c
HeroicVillain <dragonock540@gmail.com>
2020-04-27 21:21:48 -0500

    -Random Map generation, tiles assigned value & troops from noise -Enabled a game controller placeholder to see a test gamestate json

--- BackEnd/springboot_backend/src/main/java/com/anthonyOleinik/myApp/entities/GameState/GameBoard.java
+++ BackEnd/springboot_backend/src/main/java/com/anthonyOleinik/myApp/entities/GameState/GameBoard.java
@@ -1,34 +1,19 @@
 package com.anthonyOleinik.myApp.entities.GameState;
 
+import com.anthonyOleinik.myApp.Services.MapGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class GameBoard {
-    private int dimensions = (new Random().nextInt(4)*2 + 7);
+    private int dimensions;
     private List<Tile> tiles = new ArrayList<Tile>();
-
+    MapGenerator mapGenerator;
     public GameBoard(ArrayList<Integer> playerIds, int initPlayerTroops, int initAiTroops){
-        for (int i = 0; i < dimensions; i++){
-            for (int j = 0; j < dimensions; j++){
-                tiles.add(new Tile(i, j, initAiTroops));
-            }
-        }
-
-        //TODO make this take more than 4 players.
-        //currently hardcoded for 4.
-        this.getTile(0, 0).setOwner(playerIds.get(0));
-        this.getTile(0, 0).setTroops(initPlayerTroops);
-
-        this.getTile(dimensions - 1, dimensions - 1).setOwner(playerIds.get(1));
-        this.getTile(dimensions - 1, dimensions - 1).setTroops(initPlayerTroops);
-
-        this.getTile(dimensions - 1, 0).setOwner(playerIds.get(2));
-        this.getTile(dimensions - 1, 0).setTroops(initPlayerTroops);
-
-        this.getTile(0, dimensions - 1).setOwner(playerIds.get(3));
-        this.getTile(0, dimensions - 1).setTroops(initPlayerTroops);
-
+        mapGenerator = new MapGenerator(playerIds.size());
+        this.tiles = mapGenerator.CreateBoard();
+        this.dimensions = mapGenerator.dimensions;
     }
 
 
